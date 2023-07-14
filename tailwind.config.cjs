b674a7d23801677141320a7f16af7d933abd5fab
Ricardo Esparza Sanchez <richespsan.210@gmail.com>
2023-07-14 17:09:10 -0600

    adding 3 lines

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				boxing: ['Boxing', 'Adjusted Arial Fallback', 'sans-serif'],
+				'boxing-striped': ['Boxing Striped', 'Adjusted Arial Fallback', 'sans-serif'],
+			}
+		},
+	},
+	plugins: [
+		require('tailwindcss-animated')
+	],
+}
